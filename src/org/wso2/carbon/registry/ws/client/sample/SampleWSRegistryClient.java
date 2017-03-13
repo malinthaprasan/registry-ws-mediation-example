5432d0725d6b6246ca81b591f72f01b43eb83bc7
Malintha Amarasinghe <malintha.prasan@gmail.com>
2017-03-13 18:56:47 +0530

    deleted unwanted

--- src/org/wso2/carbon/registry/ws/client/sample/SampleWSRegistryClient.java
+++ src/org/wso2/carbon/registry/ws/client/sample/SampleWSRegistryClient.java
@@ -25,16 +25,24 @@ import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.registry.core.Resource;
 import org.wso2.carbon.registry.ws.client.registry.WSRegistryServiceClient;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.File;
+import java.security.cert.X509Certificate;
 
 public class SampleWSRegistryClient {
 
     private static ConfigurationContext configContext = null;
-	
+
     private static final String CARBON_HOME = ".." + File.separator + ".." + File.separator;
-    private static final String axis2Repo = CARBON_HOME + File.separator +"repository" +
+    private static final String axis2Repo = CARBON_HOME + File.separator + "repository" +
             File.separator + "deployment" + File.separator + "client";
-    private static final String axis2Conf = ServerConfiguration.getInstance().getFirstProperty("Axis2Config.clientAxis2XmlLocation");
+    private static final String axis2Conf = ServerConfiguration.getInstance()
+            .getFirstProperty("Axis2Config.clientAxis2XmlLocation");
     private static final String username = "admin";
     private static final String password = "admin";
     private static final String serverURL = "https://localhost:9443/services/";
@@ -51,149 +59,34 @@ public class SampleWSRegistryClient {
                 axis2Repo, axis2Conf);
         return new WSRegistryServiceClient(serverURL, username, password, configContext);
     }
-    
-	public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception {
         Registry registry = initialize();
         try {
             Resource resource = registry.newResource();
-            resource.setContent("<wsp:Policy xmlns:wsp=\"http://schemas.xmlsoap.org/ws/2004/09/policy\"\n"
-                    + "            xmlns:throttle=\"http://www.wso2.org/products/wso2commons/throttle\">\n"
-                    + "    <throttle:MediatorThrottleAssertion>\n"
-                    + "        <wsp:Policy>\n"
-                    + "            <!--It's possible to define a display name for a tier to show in UIs by adding the attribute of throttle:displayName=\"xxxx\" for <throttle:ID>\n"
-                    + "             element.\n"
-                    + "             For ex:  <throttle:ID throttle:type=\"ROLE\" throttle:displayName=\"xxxx\">Gold</throttle:ID>\n"
-                    + "                      ......\n"
-                    + "             -->\n"
-                    + "            <throttle:ID throttle:type=\"ROLE\">Gold</throttle:ID>\n"
-                    + "            <wsp:Policy>\n"
-                    + "                <throttle:Control>\n"
-                    + "                    <wsp:Policy>\n"
-                    + "                        <throttle:MaximumCount>20</throttle:MaximumCount>\n"
-                    + "                        <throttle:UnitTime>60000</throttle:UnitTime>\n"
-                    + "                        <!--It's possible to define tier level attributes as below for each tier level.For eg:Payment Plan for a tier\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                        <throttle:Attributes>\n"
-                    + "                            <throttle:Attribute1>xxxx</throttle:Attribute1>\n"
-                    + "                            <throttle:Attribute2>xxxx</throttle:Attribute2>\n"
-                    + "                        </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "                        -->\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                            <throttle:Attributes>\n"
-                    + "                                <throttle:x-wso2-BillingPlan>FREE</throttle:x-wso2-BillingPlan>\n"
-                    + "                                <throttle:x-wso2-StopOnQuotaReach>true</throttle:x-wso2-StopOnQuotaReach>\n"
-                    + "                            </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "\n"
-                    + "                    </wsp:Policy>\n"
-                    + "                </throttle:Control>\n"
-                    + "            </wsp:Policy>\n"
-                    + "        </wsp:Policy>\n"
-                    + "        <wsp:Policy>\n"
-                    + "            <!--It's possible to define a display name for a tier to show in UIs by adding the attribute of throttle:displayName=\"xxxx\" for <throttle:ID>\n"
-                    + "            element.\n"
-                    + "            For ex:  <throttle:ID throttle:type=\"ROLE\" throttle:displayName=\"xxxx\">Gold</throttle:ID>\n"
-                    + "                     ......\n"
-                    + "            -->\n"
-                    + "            <throttle:ID throttle:type=\"ROLE\">Silver</throttle:ID>\n"
-                    + "            <wsp:Policy>\n"
-                    + "                <throttle:Control>\n"
-                    + "                    <wsp:Policy>\n"
-                    + "                        <throttle:MaximumCount>5</throttle:MaximumCount>\n"
-                    + "                        <throttle:UnitTime>60000</throttle:UnitTime>\n"
-                    + "                        <!--It's possible to define tier level attributes as below for each tier level.For eg:Payment Plan for a tier\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                        <throttle:Attributes>\n"
-                    + "                            <throttle:Attribute1>xxxx</throttle:Attribute1>\n"
-                    + "                            <throttle:Attribute2>xxxx</throttle:Attribute2>\n"
-                    + "                        </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "                        -->\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                            <throttle:Attributes>\n"
-                    + "                                <throttle:x-wso2-BillingPlan>FREE</throttle:x-wso2-BillingPlan>\n"
-                    + "                                <throttle:x-wso2-StopOnQuotaReach>true</throttle:x-wso2-StopOnQuotaReach>\n"
-                    + "                            </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "\n"
-                    + "                    </wsp:Policy>\n"
-                    + "                </throttle:Control>\n"
-                    + "            </wsp:Policy>\n"
-                    + "        </wsp:Policy>\n"
-                    + "        <wsp:Policy>\n"
-                    + "            <!--It's possible to define a display name for a tier to show in UIs by adding the attribute of throttle:displayName=\"xxxx\" for <throttle:ID>\n"
-                    + "            element.\n"
-                    + "            For ex:  <throttle:ID throttle:type=\"ROLE\" throttle:displayName=\"xxxx\">Gold</throttle:ID>\n"
-                    + "                     ......\n"
-                    + "            -->\n"
-                    + "            <throttle:ID throttle:type=\"ROLE\">Bronze</throttle:ID>\n"
-                    + "            <wsp:Policy>\n"
-                    + "                <throttle:Control>\n"
-                    + "                    <wsp:Policy>\n"
-                    + "                        <throttle:MaximumCount>1</throttle:MaximumCount>\n"
-                    + "                        <throttle:UnitTime>60000</throttle:UnitTime>\n"
-                    + "                        <!--It's possible to define tier level attributes as below for each tier level.For eg:Payment Plan for a tier\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                        <throttle:Attributes>\n"
-                    + "                            <throttle:Attribute1>xxxx</throttle:Attribute1>\n"
-                    + "                            <throttle:Attribute2>xxxx</throttle:Attribute2>\n"
-                    + "                        </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "                        -->\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                            <throttle:Attributes>\n"
-                    + "                                <throttle:x-wso2-BillingPlan>FREE</throttle:x-wso2-BillingPlan>\n"
-                    + "                                <throttle:x-wso2-StopOnQuotaReach>true</throttle:x-wso2-StopOnQuotaReach>\n"
-                    + "                            </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "\n"
-                    + "                    </wsp:Policy>\n"
-                    + "                </throttle:Control>\n"
-                    + "            </wsp:Policy>\n"
-                    + "        </wsp:Policy>\n"
-                    + "        <wsp:Policy>\n"
-                    + "            <!--It's possible to define a display name for a tier to show in UIs by adding the attribute of throttle:displayName=\"xxxx\" for <throttle:ID>\n"
-                    + "            element.\n"
-                    + "            For ex:  <throttle:ID throttle:type=\"ROLE\" throttle:displayName=\"xxxx\">Gold</throttle:ID>\n"
-                    + "                     ......\n"
-                    + "            -->\n"
-                    + "            <throttle:ID throttle:type=\"ROLE\">Unauthenticated</throttle:ID>\n"
-                    + "            <wsp:Policy>\n"
-                    + "                <throttle:Control>\n"
-                    + "                    <wsp:Policy>\n"
-                    + "                        <throttle:MaximumCount>10000</throttle:MaximumCount>\n"
-                    + "                        <throttle:UnitTime>60000</throttle:UnitTime>\n"
-                    + "                        <!--It's possible to define tier level attributes as below for each tier level.For eg:Payment Plan for a tier\n"
-                    + "                        <wsp:Policy>\n"
-                    + "                        <throttle:Attributes>\n"
-                    + "                            <throttle:Attribute1>xxxx</throttle:Attribute1>\n"
-                    + "                            <throttle:Attribute2>xxxx</throttle:Attribute2>\n"
-                    + "                        </throttle:Attributes>\n"
-                    + "                        </wsp:Policy>\n"
-                    + "                        -->\n"
-                    + "                    </wsp:Policy>\n"
-                    + "                </throttle:Control>\n"
-                    + "            </wsp:Policy>\n"
-                    + "        </wsp:Policy>\n"
-                    + "    </throttle:MediatorThrottleAssertion>\n"
-                    + "</wsp:Policy>\n");
+            resource.setContent("<sequence xmlns=\"http://ws.apache.org/ns/synapse\" name=\"log_in_message\">\n"
+                    + "    <log level=\"full\">\n"
+                    + "        <property name=\"IN_MESSAGE\" value=\"IN_MESSAGE\"/>\n"
+                    + "        <property name=\"IN_MESSAGE123\" value=\"IN_MESSAGE123\"/>\n"
+                    + "    </log>\n"
+                    + "</sequence>");
 
-            String resourcePath = "/_system/governance/apimgt/applicationdata/tiers.xml";
+            String resourcePath = "/_system/governance/apimgt/customsequences/in/log_in_message.xml";
             registry.put(resourcePath, resource);
 
             System.out.println("A resource added to: " + resourcePath);
 
-            Resource getResource = registry.get("/_system/governance/apimgt/applicationdata/tiers.xml");
+            Resource getResource = registry.get(resourcePath);
             System.out.println("Resource retrived");
             System.out.println("Printing retrieved resource content: " +
-                    new String((byte[])getResource.getContent()));
+                    new String((byte[]) getResource.getContent()));
 
         } finally {
             //Close the session
-            ((WSRegistryServiceClient)registry).logut();
+            ((WSRegistryServiceClient) registry).logut();
         }
-		System.exit(0);
-		
-	}
+        System.exit(0);
+
+    }
+
 }
